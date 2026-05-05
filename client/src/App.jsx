31439bcc7b8d504460108805570568d1879a486f
Khadija Bilal <khadija.bilal9167@gmail.com>
2026-05-05 23:55:20 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import SplashScreen       from "./pages/SplashScreen";
 import AuthScreen         from "./pages/AuthScreen";
 import PersonalInfoScreen from "./pages/PersonalInfoScreen";
 import ProfileDisplay     from "./pages/ProfileDisplay";
-import MediTrackDashboard from "./pages/dashboard/dashboards/MediTrackDashboard";
-import CaregiverDashboard from "./pages/dashboard/dashboards/CaregiverDashboard";
+import MediTrackDashboard from "./pages/dashboard/MediTrackDashboard";
+import CaregiverDashboard from "./pages/dashboard/CaregiverDashboard";
 import { getMedicationsAPI }  from "./api/medicationAPI";
 import { getPatientDataAPI }  from "./api/caregiverAPI";
 
